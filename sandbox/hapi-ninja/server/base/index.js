bf670aac4f180f886efef644ff6e882c9f03fb27
Zhaoxin Sun <sunzhx.js@gmail.com>
2015-08-05 00:08:38 -0500

    play with mongoose in project sandbox

--- sandbox/hapi-ninja/server/base/index.js
+++ sandbox/hapi-ninja/server/base/index.js
@@ -1,4 +1,5 @@
 // Base routes for default index/root path, about page, 404 error pages, and others..
+var UserCtrl = require('../controller/server.controller.js');
 exports.register = function (server, options, next) {
 
     server.route([
@@ -19,7 +20,48 @@ exports.register = function (server, options, next) {
             }
         },
 
+        //A temporary Get methods (for browser easy test) to test write operation 
+        //and verify if the model is correct.
+        {
+            method: 'GET',
+            path:'/PostArray/{user}',
+            config:{ 
+                handler: function(request, reply) {
+                    var i,j;
+                    var data={};
+                    
+                    //Test data
+                    var pointX= [1,2,3,4];
+                    var pointY= [5,6,7,8];
+                    data.sigs = new Array();
+                    //Construct data object
+                    for(j=0;j<5;j++){
+                        data.sigs.push({deltax:pointX,deltay:pointY});
+                        }
+                    //using URL param "user" to determine username
+                    data.username=request.params.user;
+                
+                    //Pass data to model's write operation.
+                    UserCtrl.create(request,reply,data);
+                    //Send response to user after finish writing
+                    
+                },
+                id: 'PostArray'
+            }
+        },
 
+        //a temporary GET methods to retrieve all users' information stored in DB
+        {
+            method: 'GET',
+            path:'/GetArray',
+            config:{
+                handler: function(request,reply){
+                    UserCtrl.list(request,reply);
+                    
+                },
+                id: 'GetArray'
+            } 
+        },
 
         {
             method: 'GET',
