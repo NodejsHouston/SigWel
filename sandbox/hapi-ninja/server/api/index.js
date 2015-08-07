a97af324420e4e25ef4e95f1b421d7e1178b651d
Zhaoxin Sun <sunzhx.js@gmail.com>
2015-08-06 23:55:08 -0500

    module and api test

--- sandbox/hapi-ninja/server/api/index.js
+++ sandbox/hapi-ninja/server/api/index.js
@@ -1,4 +1,18 @@
 // API routes
+var vector ={agent: "myagent"};
+vector.init = function(data,fn){
+        var err;
+        if(data<5){
+            err = false;
+            data=10;
+        }
+        else err=true;
+
+        if (fn(err,data))
+            return this;
+        else return false;
+};
+
 exports.register = function(server, options, next){
 
     var apiBase = '/api';
@@ -11,7 +25,7 @@ exports.register = function(server, options, next){
                 handler: function(request, reply){
                     reply({
                         message: 'blah test'
-                    })；
+                    })
                 },
                 id: 'blah'
             }
@@ -21,10 +35,31 @@ exports.register = function(server, options, next){
             path: apiBase + '/user/{username}',
             config:{
                 handler: function(request,reply){
-                    var data = request.payload.data;
+                    var i,initSigCollection;
+                    initSigCollection= new Array();
+                    //var UninitSigCollection = request.payload.data;
+                    var UninitSigCollection=[1,2,3,4,6,4];
+                    var isOK = UninitSigCollection.every(function(sig){
+                        return vector.init(sig, function(err,initsig){
+                            if (err) {
+                                console.log("init failed");
+                               // reply("init failed");
+                                return false;
+                            }
+                            else {
+                                initSigCollection.push(initsig);
+                                return true;
+                            }
+                        });
+                       
+                    });
+                    if (isOK) reply("that is good");
+                    else reply("init failed HAHAHA");
+                    console.log(initSigCollection);
+                    
                     var username= request.payload.username;
                     console.log(username);
-                    console.log(data);
+                    //console.log(data);
 
                 },
                 id: 'tuser'
