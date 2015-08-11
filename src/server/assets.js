d209bcc3ec6b2183f672174c14a2315af1815d28
Jeffry Tupa <jeffry@tupadesigns.com>
2015-08-10 23:45:05 -0500

    Cleaned up files and added a registration page

--- src/server/assets.js
+++ src/server/assets.js
@@ -1,8 +1,8 @@
 // assets to be used by the 'hapi-assets' module based on process.env.NODE_ENV
 module.exports = {
     development: {
-        js: ['js/test-one.js', 'js/test-two.js'],
-        css: ['css/test-one.css', 'css/test-two.css']
+        js: ['js/lib/modernizr.js', 'js/lib/jquery.js', 'js/lib/foundation.min.js', 'js/custom/index.js'],
+        css: ['css/foundation.css','css/styles.css']
     },
     production: {
         js: ['js/scripts.js'],
