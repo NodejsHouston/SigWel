7e7934c8a5ae9f9cadb96cd43f8309ada2372ce8
Jeffry Tupa <jeffry@tupadesigns.com>
2015-08-13 23:43:30 -0500

    reverted back the changes to the default task

--- gulpfile.js
+++ gulpfile.js
@@ -10,32 +10,31 @@ var port = process.env.PORT || config.defaultPort;
 
 // the default task that is run with the command 'gulp'
 gulp.task('default', function () {
-    gulp.start('serve-dev');
-    // // assets is where you define your application assets and you can pass them into gulp.
-    // var assets = require('./src/assets');
+    // assets is where you define your application assets and you can pass them into gulp.
+    var assets = require('./src/assets');
     
-    // // change the working directory to the public folder, where your assets are located.
-    // var gulpFileCwd = __dirname + '/src/public';
-    // process.chdir(gulpFileCwd);
-    // // print the working directory
-    // $.util.log('Working directory changed to', $.util.colors.magenta(gulpFileCwd));
+    // change the working directory to the public folder, where your assets are located.
+    var gulpFileCwd = __dirname + '/src/public';
+    process.chdir(gulpFileCwd);
+    // print the working directory
+    $.util.log('Working directory changed to', $.util.colors.magenta(gulpFileCwd));
 
-    // // concat and minify your css
-    // gulp.src(assets.development.css)
-    //     .pipe($.concat('styles.css'))
-    //     .pipe($.minifyCss())
-    //     .pipe(gulp.dest('./css/'));
+    // concat and minify your css
+    gulp.src(assets.development.css)
+        .pipe($.concat('styles.css'))
+        .pipe($.minifyCss())
+        .pipe(gulp.dest('./css/'));
 
-    // // concat and minify your js
-    // gulp.src(assets.development.js)
-    //     .pipe($.concat('scripts.js'))
-    //     .pipe($.uglify())
-    //     .pipe(gulp.dest('./js/'));
+    // concat and minify your js
+    gulp.src(assets.development.js)
+        .pipe($.concat('scripts.js'))
+        .pipe($.uglify())
+        .pipe(gulp.dest('./js/'));
 
-    // // optimize your images
-    // gulp.src('./images/*')
-    //     .pipe($.imagemin())
-    //     .pipe(gulp.dest('./images/'));
+    // optimize your images
+    gulp.src('./images/*')
+        .pipe($.imagemin())
+        .pipe(gulp.dest('./images/'));
 
 });
 
