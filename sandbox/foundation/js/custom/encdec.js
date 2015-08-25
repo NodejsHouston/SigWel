0ea21dc02faf11b101bc2e4b92309bef265e40b4
Il Mo Jung <ijung5399@gmail.com>
2015-08-25 07:18:31 -0500

    added all fx to encdec.js

--- sandbox/foundation/js/custom/encdec.js
+++ sandbox/foundation/js/custom/encdec.js
@@ -1,8 +1,8 @@
-// Part of https://github.com/chris-rock/node-crypto-examples
+// reference:   https://github.com/chris-rock/node-crypto-examples
 
 var crypto = require('crypto'),
     algorithm = 'aes-256-ctr',
-    password = 'S1gW@1Passw0rd',
+    password = 'S1gW@1Passw0rd', // Sig proj specific
 	fs = require('fs');
 
 function Crypto(key) {
@@ -11,16 +11,30 @@ function Crypto(key) {
 	}
 }
 
-
-function encrypt(stream) {
+function encryptstream(stream) {
 	var encryptor = crypto.createCipher(algorithm, password);
 	return stream.pipe(encryptor);
 }
 
-function decrypt(stream) {
+function decryptstream(stream) {
 	var decryptor = crypto.createDecipher(algorithm, password);
 	return stream.pipe(decryptor);
 }
+
+function encrypt(buffer){
+	  var cipher = crypto.createCipher(algorithm,password)
+		    var crypted = Buffer.concat([cipher.update(buffer),cipher.final()]);
+	    return crypted;
+}
+ 
+function decrypt(buffer){
+	  var decipher = crypto.createDecipher(algorithm,password)
+		    var dec = Buffer.concat([decipher.update(buffer) , decipher.final()]);
+	    return dec;
+}
+Crypto.prototype.EncryptStream = encryptstream;
+Crypto.prototype.DecryptStream = decryptstream;
+
 Crypto.prototype.Encrypt = encrypt;
 Crypto.prototype.Decrypt = decrypt;
 module.exports = Crypto;
