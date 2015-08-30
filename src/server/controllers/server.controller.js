ec1b9c3412a327da78b521198596b185939a5236
ijung5399 <daebarkee@gmail.com>
2015-08-30 16:59:00 -0500

    debug fix

--- src/server/controllers/server.controller.js
+++ src/server/controllers/server.controller.js
@@ -8,17 +8,16 @@ exports.CreateUser = function (request,reply,data){
 	var newuser = new user({
 		Username: data.Username,
 		Email: crypto.Encrypt(data.Email),
-		SigSet: crypto.Encrypt(JSON.stringify(data.Sigs),
+		SigSet: crypto.Encrypt(JSON.stringify(data.Sigs)),
 		NormalizeBase: data.NormalizeBase
 	});
 	newuser.save(function(err){
 		
 		if (err)
 			reply({type:false, message: "Failed to save user information..."});
-		//console.log("win");
+		console.log("win");
 		reply({type:true, message: "Save user information successfully!"});
 	});
-
 };
 
 //DB read operation, read certain user's information from DB and send back to client
@@ -31,7 +30,7 @@ exports.FindUser = function(request,reply,data,fn){
 			reply({type:false, message:"Cannot read this user's information."});
 		}
 		result.Email = crypto.Decrypt(result.Email);
-		result.SigSet = crypto.Decrypt(JSON.parse(result.SigSet);
+		result.SigSet = crypto.Decrypt(JSON.parse(result.SigSet));
 		fn(results);
 		
 	});
